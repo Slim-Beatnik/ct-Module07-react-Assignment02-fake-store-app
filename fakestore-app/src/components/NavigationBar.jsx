529ee4df904e6cfc05e6e74d280701ab6ceb0d72
Slim-Beatnik <totem64@gmail.com>
2025-05-25 12:55:35 -0700

    netlify svg test 2

--- fakestore-app/src/components/NavigationBar.jsx
+++ fakestore-app/src/components/NavigationBar.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '/store-svgrepo-com.svg';
+import logo from 'store-svgrepo-com.svg';
 
 
 function NavigationBar() {
